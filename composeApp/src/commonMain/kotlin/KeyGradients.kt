89b6b5a5e7d5fa8bb45bd68b0a3ce417a3db8da7
Jaewon Lee <jwleetop@clebrain.com>
2024-06-19 17:51:53 +0900

    fix: fix key gradients

--- composeApp/src/commonMain/kotlin/KeyGradients.kt
+++ composeApp/src/commonMain/kotlin/KeyGradients.kt
@@ -6,19 +6,19 @@ import androidx.compose.ui.graphics.Color
 fun blackUnpressedBase(): Brush {
     return linearGradient(
         colorStops = arrayOf(
-            Pair(0f, Color(0xFF3d3e40)),
-            Pair(1f, Color(0xFF3f4042)),
-            Pair(0f, Color(0xFF414244)),
-            Pair(1f, Color(0xFF6f7374)),
-        ), start = Offset(1f, 0.477f), end = Offset(-0.643f, 0.485f)
+            Pair(0f, Color(0xFF3D3E40)),
+            Pair(.044f, Color(0xFF3F4042)),
+            Pair(.084f, Color(0xFF414244)),
+            Pair(1f, Color(0xFF6F7374)),
+        ), start = Offset(1f, 0.477f), end = Offset(-0.63f, 0.485f)
     )
 }
 
 fun blackUnpressedTop(): Brush {
     return linearGradient(
         colorStops = arrayOf(
-            Pair(0f, Color(0xFF5d5d5d)),
-            Pair(0.044f, Color(0xFF272727)),
+            Pair(0f, Color(0xFF5D5B5D)),
+            Pair(1f, Color(0xFF272727)),
         ), start = Offset(0.5f, 0f), end = Offset(0.5f, 1f)
     )
 }
@@ -26,26 +26,26 @@ fun blackUnpressedTop(): Brush {
 fun blackUnpressedBottom(): Brush {
     return linearGradient(
         colorStops = arrayOf(
-            Pair(0f, Color(0xFF5a5b5d)),
-            Pair(1f, Color(0xFF2f3032)),
-        ), start = Offset(1f, 0.477f), end = Offset(-.063f, .485f)
+            Pair(0f, Color(0xFF5A5B5D)),
+            Pair(1f, Color(0xFF2F3032)),
+        ), start = Offset(0.5f, 0.0f), end = Offset(0.5f, 1f)
     )
 }
 fun blackPressedBase(): Brush {
     return linearGradient(
         colorStops = arrayOf(
             Pair(0f, Color(0xFF303031)),
-            Pair(1f, Color(0xFF282829)),
-            Pair(0f, Color(0xFF282829)),
+            Pair(.044f, Color(0xFF282829)),
+            Pair(.084f, Color(0xFF282829)),
             Pair(1f, Color(0xFF424445)),
-        ), start = Offset(0.528f, -0.13f), end = Offset(-0.5f, 1f)
+        ), start = Offset(1f, .477f), end = Offset(-0.063f, .485f)
     )
 }
 
 fun blackPressedBottom(): Brush {
     return linearGradient(
         colorStops = arrayOf(
-            Pair(0f, Color(0xFF4c4d4f)),
+            Pair(0f, Color(0xFF4C4D4F)),
             Pair(1f, Color(0xFF)),
         ), start = Offset(0.528f, -0.13f), end = Offset(-0.5f, 1f)
     )
@@ -56,7 +56,7 @@ fun blackPressedTop(): Brush {
     return linearGradient(
         colorStops = arrayOf(
             Pair(0f, Color(0xFF434343)),
-            Pair(0.044f, Color(0xFF1a1a1a)),
+            Pair(0.044f, Color(0xFF1A1A1A)),
         ), start = Offset(0.5f, 0f), end = Offset(0.5f, 1f)
     )
 }
@@ -65,7 +65,7 @@ fun blackPressedTop(): Brush {
 fun whiteCenterUnpressed(): Brush {
     return linearGradient(
         colorStops = arrayOf(
-            Pair(0f, Color(0xFFeaeaea)),
+            Pair(0f, Color(0xFFEAEAEA)),
             Pair(1f, Color(0xFFf2f2f2)),
         ), start = Offset(1f, .161f), end = Offset(0.091f, 0.803f)
     )
@@ -74,8 +74,8 @@ fun whiteCenterUnpressed(): Brush {
 fun whiteCenterPressed(): Brush {
     return linearGradient(
         colorStops = arrayOf(
-            Pair(0f, Color(0xFFc5c4c9)),
-            Pair(1f, Color(0xFFc8c7cc)),
+            Pair(0f, Color(0xFFC5C4C9)),
+            Pair(1f, Color(0xFFC8C7CC)),
         ), start = Offset(1f, .161f), end = Offset(0.091f, 0.803f)
     )
 }
