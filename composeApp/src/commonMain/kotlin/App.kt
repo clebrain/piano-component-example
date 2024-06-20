411072ad378598ece5629778425ec1c6f08969b6
Jaewon Lee <jwleetop@clebrain.com>
2024-06-20 18:32:46 +0900

    feat: apply press piano

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,32 +1,11 @@
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.awaitEachGesture
-import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.withTransform
-import androidx.compose.ui.input.pointer.PointerInputChange
-import androidx.compose.ui.input.pointer.PointerInputScope
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalHapticFeedback
-import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.coroutineScope
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import util.isDiatonic
+import piano.Piano
 
 @Composable
 @Preview
@@ -49,97 +28,5 @@ fun App() {
 }
 
 
-data class KeyInfo(
-    val midiKey: Int,
-    var pressed: MutableState<Boolean>
-)
-
-suspend fun PointerInputScope.detectPressGestures(
-    onPressStart: ((Offset) -> Unit)? = null,
-    onPressEnd: (() -> Unit)? = null,
-) = coroutineScope {
-    awaitEachGesture {
-        val firstDown = awaitFirstDown()
-        firstDown.consume()
-        onPressStart?.invoke(firstDown.position)
-
-        // TapGestureDetector - AwaitPointerEventScope.consumeUntilUp()
-        do {
-            val event = awaitPointerEvent()
-            event.changes.forEach { changeEvent: PointerInputChange -> changeEvent.consume() }
-        } while (event.changes.any { it.pressed })
-
-        onPressEnd?.invoke()
-    }
-}
-
-@Composable
-fun Piano(
-    range: IntRange,
-    keyPressedList: List<Boolean>,
-) {
-    val length = range.last
-    val keys by remember {
-        mutableStateOf(
-            if (keyPressedList.size != 128) {
-                List(length) {
-                    KeyInfo(midiKey = it + range.first, mutableStateOf(false))
-                }
-            } else {
-                List(length) {
-                    KeyInfo(
-                        midiKey = it + range.first,
-                        mutableStateOf(keyPressedList[it + range.first])
-                    )
-                }
-            }
-        )
-    }
-    val basis = getKeyPosition(keys.first());
-
-    BoxWithConstraints(Modifier.height(450.dp).background(Color.Black)) {
-        Canvas(Modifier.fillMaxSize().pointerInput(Unit) {
-            detectTapGestures { }
-            detectPressGestures(
-                onPressStart = { offset ->
-                    keys.forEachIndexed { _, keyInfo ->
-                        val diatonic = isDiatonic(keyInfo.midiKey)
-                        val keyOffset = getKeyPosition(keyInfo).toFloat() - basis.toFloat()
-                        val detectKeyArea = Rect(
-                            Offset(keyOffset, 0f),
-                            Offset(
-                                keyOffset + 90f,
-                                if (diatonic) maxHeight.value else maxHeight.value * 0.7f
-                            )
-                        )
-                        if (detectKeyArea.contains(offset)) {
-                            keyInfo.pressed.value = !keyInfo.pressed.value
-                            return@detectPressGestures
-                        }
-                    }
-                },
-                onPressEnd = {
-                    keys.forEachIndexed { _, keyInfo ->
-                        keyInfo.pressed.value = false
-                    }
-                }
-            )
-        }) {
-            keys.forEachIndexed { idx, keyInfo ->
-                withTransform({
-                    translate(left = getKeyPosition(keyInfo).toFloat() - basis.toFloat())
-                }) {
-                    drawKey(
-                        midiKey = keyInfo.midiKey,
-                        isPressed = keyInfo.pressed.value,
-                        isLeftEnd = idx == 0,
-                        isRightEnd = idx == keys.size - 1,
-                        maxHeight = maxHeight.value
-                    )
-                }
-            }
-        }
-    }
-}
 
 
